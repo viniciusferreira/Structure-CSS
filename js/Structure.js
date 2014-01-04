65a384a7b401259e4143ae3e1a7f6da7173c7ab7
Valdecir Junior <valdecirtjunior@gmail.com>
2014-01-03 22:18:10 -0200

    1.0

---
+++ js/Structure.js
@@ -0,0 +1,46 @@
+(function (window, document) {
+
+    var layout   = document.getElementById('layout'),
+        menu     = document.getElementById('menu'),
+        menuLink = document.getElementById('menuLink');
+        navbarLink = document.getElementById('navbarLink');
+
+    function toggleClass(element, className) {
+        var classes = element.className.split(/\s+/),
+            length = classes.length,
+            i = 0;
+
+        for(; i < length; i++) {
+          if (classes[i] === className) {
+            classes.splice(i, 1);
+            break;
+          }
+        }
+        if (length === classes.length) {
+            classes.push(className);
+        }
+
+        element.className = classes.join(' ');
+    }
+
+    menuLink.onclick = function (e) {
+        var active = 'active';
+
+        e.preventDefault();
+        toggleClass(layout, active);
+        toggleClass(menu, active);
+        toggleClass(menuLink, active);
+        toggleClass(navbar, active);
+    };
+
+
+
+    
+    navbarLink.onclick = function (e) {
+        var active = 'active';
+
+        e.preventDefault();
+        toggleClass(navbar, active);
+    };
+
+}(this, this.document));
